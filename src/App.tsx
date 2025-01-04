37a6999c9e1ac0650b84140543ce33a99044c6ca
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2025-01-04 21:07:20 +0100

    add basic auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+// App.js
+
+import { useAuth } from 'react-oidc-context';
+import { cognitoClientId, cognitoDomain, logoutUri } from './cognitoAuthConfig';
+
 function App() {
-  return <div>Hello World</div>;
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    window.location.href = `${cognitoDomain}/logout?client_id=${cognitoClientId}&logout_uri=${encodeURIComponent(
+      logoutUri,
+    )}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
