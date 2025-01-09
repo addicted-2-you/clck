032e496e906c29e17d21acea5ef318963ae276bf
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2025-01-09 22:05:10 +0100

    update layout

--- web/src/Layout.tsx
+++ web/src/Layout.tsx
@@ -1,8 +1,10 @@
 import { useAuth } from 'react-oidc-context';
-import { Outlet } from 'react-router';
+import { NavLink, Outlet } from 'react-router';
 
 function Layout() {
   const auth = useAuth();
+
+  const onUsernameClick = () => {};
 
   if (auth.isLoading) {
     return <div>Loading...</div>;
@@ -12,25 +14,54 @@ function Layout() {
     return <div>Encountering error... {auth.error.message}</div>;
   }
 
-  if (auth.isAuthenticated) {
-    return (
-      <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-        <button onClick={() => auth.removeUser()}>Sign out</button>
-
-        <Outlet />
-      </div>
-    );
-  }
-
   return (
-    <div>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+    <div className="h-screen w-full overflow-hidden">
+      <header className="px-5 py-1 flex justify-end border-b-2 border-black">
+        <div className="flex gap-5">
+          {auth.isAuthenticated ? (
+            <>
+              <button
+                className="hover:underline active:opacity-50"
+                onClick={onUsernameClick}
+              >
+                {auth.user?.profile.email}
+              </button>
+              <button
+                className="hover:underline active:opacity-50"
+                onClick={() => auth.removeUser()}
+              >
+                Sign out
+              </button>
+            </>
+          ) : (
+            <button
+              className="hover:underline active:opacity-50"
+              onClick={() => auth.signinRedirect()}
+            >
+              Sign in
+            </button>
+          )}
+        </div>
+      </header>
       <Outlet />
+      <footer className="absolute right-0 bottom-0 left-0 mx-auto px-5 py-1 w-fit flex gap-5 border-t-2 border-black">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `${isActive ? 'text-white bg-black' : ''} px-2 hover:underline`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/leaderboard"
+          className={({ isActive }) =>
+            `${isActive ? 'text-white bg-black' : ''} px-2 hover:underline`
+          }
+        >
+          Leaderboard
+        </NavLink>
+      </footer>
     </div>
   );
 }
